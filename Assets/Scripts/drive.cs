8d1224625a506512f7fb7189038c6d7fe72d4a19
Dakota Herold <dakota.herold@yahoo.com>
2016-03-04 19:55:26 -0500

    Updated car with Unity Standard Assets for better functionality. Full controller support included. Please see scripts to understand how things will be implemented in DirectX version.

---
+++ Assets/Scripts/drive.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class drive : MonoBehaviour {
+
+	private float speed = 1.0f;
+	private float topSpeed = 1.0f;
+
+	private Rigidbody r;
+
+	private Vector3 velocity;
+	private Vector3 acceleration;
+	private Vector3 direction;
+
+	// Use this for initialization
+	void Start () {
+		velocity = new Vector3 (0, 0, 0);
+		acceleration = new Vector3 (0, 0, 0);
+		direction = transform.forward;
+		//r = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		acceleration *= 0.9f;
+
+		if (Input.GetKey ("up")) {
+			acceleration += direction * speed; //* Time.deltaTime;
+		}
+		if (Input.GetKey ("down")) {
+			acceleration += -0.5f * direction * speed; //* Time.deltaTime;
+		}
+
+		if(Input.GetKey("left")){
+			direction = Quaternion.AngleAxis (-5,Vector3.up) * direction;
+			transform.rotation = (Quaternion.LookRotation (direction));
+			//transform.Rotate (new Vector3(0,-1.0f,0));
+		}
+		if(Input.GetKey("right")){
+			direction = Quaternion.AngleAxis (5,Vector3.up) * direction;
+			transform.rotation = (Quaternion.LookRotation (direction));
+			//transform.Rotate (new Vector3(0,1.0f,0));
+		}
+		/*
+		velocity *= 0.9f;
+		if (velocity.magnitude < 0.001f * Time.deltaTime) {
+			velocity *= 0.0f;
+		}
+		*/
+
+		if (velocity.magnitude < 0.01f) {
+			velocity *= 0.0f;
+		}
+		if (acceleration.magnitude < 0.01f) {
+			acceleration *= 0.0f;
+		} else if (acceleration.magnitude > topSpeed) {
+			acceleration.Normalize ();
+			acceleration *= topSpeed;
+		}
+		velocity -= velocity*0.1f;
+		velocity += acceleration;
+		transform.position += velocity * Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/fakeBumb.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fakeBumb : MonoBehaviour {
+	public GameObject player;
+
+	float range = 5;
+	public float yi;
+	// Use this for initialization
+	void Start () {
+		yi = player.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var dy = 0.25f;
+		float distance = (transform.position - player.transform.position).magnitude;
+		if (distance < range) {
+			if (player.transform.position.y < dy+yi) {
+				player.transform.Translate (new Vector3 (0, dy, 0));
+			}
+		} else if (distance == range) {
+			//Quaternion.r
+			if (player.transform.position.y > yi) {
+				player.transform.Translate (new Vector3 (0, -dy, 0));
+			}
+		} else {
+			if (player.transform.position.y > yi) {
+				player.transform.Translate (new Vector3 (0, -dy, 0));
+			}
+		}
+			
+	}
+}
